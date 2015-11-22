79ae5d20b75001b172cecf3d774cce1a6817008e
unknown <sherif.ihab.94@gmail.com>
2015-11-21 18:52:39 -0800

    Renames profile to edit profile

---
+++ app/src/main/java/com/example/alyezz/beygollak/EditProfile.java
@@ -0,0 +1,55 @@
+package com.example.alyezz.beygollak;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditProfile extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button bUpdate, bLogout;
+    EditText etFirstName, etLastName, etLocation;
+    TextView etDateOfBirth, etGender;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        etFirstName = (EditText) findViewById(R.id.etFirstName);
+//        etLastName = (EditText) findViewById(R.id.etLastName);
+//        etLocation = (EditText) findViewById(R.id.etLocation);
+//        etDateOfBirth = (TextView) findViewById(R.id.etDateOfBirth);
+//        etGender = (TextView) findViewById(R.id.etGender);
+        bLogout = (Button) findViewById(R.id.bLogout);
+        bUpdate = (Button) findViewById(R.id.bUpdate);
+
+
+        bLogout.setOnClickListener(this);
+        bUpdate.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId())
+        {
+            case R.id.bLogout:
+
+                startActivity(new Intent(this, Login.class));
+
+                break;
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/alyezz/beygollak/login.java
+++ app/src/main/java/com/example/alyezz/beygollak/login.java
@@ -4,8 +4,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class login extends AppCompatActivity implements View.OnClickListener {
+public class Login extends AppCompatActivity implements View.OnClickListener {
 
     Button bLogin;
     EditText etEmail, etPassword;
@@ -64,13 +62,13 @@ public class login extends AppCompatActivity implements View.OnClickListener {
                 }
                 else
                 {
-                    startActivity(new Intent(this, Profile.class));
+                    startActivity(new Intent(this, EditProfile.class));
                 }
                 break;
 
             case R.id.tvRegisterLink:
 
-                startActivity(new Intent(this, register.class));
+                startActivity(new Intent(this, Register.class));
                 break;
         }
 
